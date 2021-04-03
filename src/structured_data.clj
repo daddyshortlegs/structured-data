380d719edc4be640db09991faa35d02ceb500e00
andys <andysmith836@gmail.com>
2021-04-03 21:26:10 +0100

    Did a few exercises

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,25 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [exp (+ x x)]
+    (Math/pow exp exp))
+  )
 
 (defn spiff [v]
-  :-)
+  (let [item1 (get v 0)
+        item2 (get v 2)]
+    (+ item1 (if (= nil item2) 0 item2))
+    )
+  )
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[item1 _ item2] v]
+    (+ item1 (if (= nil item2) 0 item2))
+    )
+  )
 
 (defn point [x y]
   [x y])
@@ -19,37 +28,52 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[point1 point2] rectangle
+        [x1 _] point1
+        [x2 _] point2]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[point1 point2] rectangle
+        [_ y1] point1
+        [_ y2] point2]
+    (- y2 y1)
+    )
+  )
 
 (defn square? [rectangle]
-  :-)
+  (= (width rectangle) (height rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[point1 point2] rectangle
+        [x1 y1] point1
+        [x2 y2] point2
+        [x3 y3] point]
+    (and (<= x1 x3 x2) (<= y1 y3 y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[point1 point2] inner]
+    (and (contains-point? outer point1) (contains-point? outer point2))))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (author-count book) 1))
 
 (defn add-author [book new-author]
-  :-)
+  (let [authors (:authors book)
+        new-authors (conj authors new-author)]
+    (assoc book :authors new-authors)))
 
 (defn alive? [author]
-  :-)
+  (not (contains? author :death-year)))
 
 (defn element-lengths [collection]
   :-)
